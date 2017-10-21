7747aa3dca24a65a2e190f1f3a4b6d50aea86a02
Zhang Shuo <zhangshuocn@gmail.com>
2017-10-22 00:09:06 +0800

    Initial commit

---
+++ proj04-common/src/com/zhangshuo/DataStructure1.java
@@ -0,0 +1,13 @@
+package com.zhangshuo;
+
+public class DataStructure1 {
+    public String getMember1() {
+        return member1;
+    }
+
+    public void setMember1(String member1) {
+        this.member1 = member1;
+    }
+
+    private String member1 = "";
+}
---
+++ proj04-core/src/com/zhangshuo/EntryPoint.java
@@ -0,0 +1,7 @@
+package com.zhangshuo;
+
+public class EntryPoint {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
